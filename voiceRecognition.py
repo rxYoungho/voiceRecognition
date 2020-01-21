551f1c9ce7676110f9b7e5ea52bbd710fcce99bd
Youngho Kim <youngho.kim@stonybrook.edu>
2020-01-21 15:38:04 +0900

    Update voiceRecognition.py

--- voiceRecognition.py
+++ voiceRecognition.py
@@ -2,17 +2,17 @@ import webbrowser
 import speech_recognition as sr
 
 r = sr.Recognizer()
-r1 = sr.Recognizer()
+
 with sr.Microphone() as source:
     print("Speak Anything :")
     audio = r.listen(source)
     text = r.recognize_google(audio)
-    try:
-        print("You said : {}".format(text))
-        if 'Google' or 'google' in text:
-            url = "http://www.google.com"
-            webbrowser.open(url) #This opens without any condition!!!! error!!
-    except:
+    
+    print("You said : {}".format(text))
+    if 'Google' or 'google' in text:
+        url = "http://www.google.com"
+        webbrowser.open(url) #This opens without any condition!!!! error!!
+    else:
         print("Sorry could not recognize what you said")
     
 # with sr.Microphone() as source:
