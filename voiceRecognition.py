9a14b59048d7392213b147af24c654d5e428debe
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-01-26 18:01:44 +0900

    Update voiceRecognition.py

--- voiceRecognition.py
+++ voiceRecognition.py
@@ -7,45 +7,60 @@ def voiceCon(): #will call this when the button is clicked
     repeat = 1
     r = sr.Recognizer()
     with sr.Microphone() as source:  #this code uses the mic.
+        r.energy_threshold = 9001
         print("Speak Anything :")
-        audio = r.listen(source) #this code saves the word that was given from the user (speech) to "text"
-        text = r.recognize_google(audio)
+        try:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source) #this code saves the word that was given from the user (speech) to "text"
+            text = r.recognize_google(audio)
 
-        print(text)
+            print(text)
 
-        if text == "Google" or text == "google": 
-            url = "http://www.google.com"
-            webbrowser.open(url) 
-    
-            #text = "" #must be initialized, since the text saves the previous speech to text
-    
-        if text == "webtoon" or text == "Webtoon":
-            url = "https://comic.naver.com/webtoon/list.nhn?titleId=670143&weekday=wed"
-            webbrowser.open(url) 
-            #text = "" 
+            if text == "Google" or text == "google": 
+                url = "http://www.google.com"
+                webbrowser.open(url) 
         
-        if text == "Naver" or text == "naver":
-            url = "http://www.naver.com"
-            webbrowser.open(url)
-            #text = "" 
+                #text = "" #must be initialized, since the text saves the previous speech to text
         
-        if text == 'youtube' or text == 'YouTube':
-            url = "http://www.youtube.com"
-            webbrowser.open(url)
-            #text = ""
+            if text == "webtoon" or text == "Webtoon":
+                url = "https://comic.naver.com/webtoon/list.nhn?titleId=670143&weekday=wed"
+                webbrowser.open(url) 
+                #text = "" 
+            
+            if text == "Naver" or text == "naver":
+                url = "http://www.naver.com"
+                webbrowser.open(url)
+                #text = "" 
+            
+            if text == 'youtube' or text == 'YouTube':
+                url = "http://www.youtube.com"
+                webbrowser.open(url)
+                #text = ""
 
-        if text =='github' or text =='Github':
-            url = "https://github.com/RelaxDanny?tab=repositories"
-            webbrowser.open(url)
-            #text = "" 
+            if text =='github' or text =='Github':
+                url = "https://github.com/RelaxDanny?tab=repositories"
+                webbrowser.open(url)
+                #text = "" 
 
+            if text == 'Yeri' or text == 'yeri':
+                url = 'https://www.chanel.com/ko_KR/'
+                webbrowser.open(url)
+        
+            if text == 'my grade' or text == 'My grade':
+                url = 'https://psns.cc.stonybrook.edu/psp/csprods/EMPLOYEE/CAMP/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?'
+                webbrowser.open(url)
+            else:
+                text = ""
+                #if nothing is asked or weird things are asked, exit.
+                #text = ""
+        except sr.UnknownValueError:
+            tkinter.messagebox.showinfo("Sorry please speak again.")
 
-
-        else:
-            text = ""
-            #if nothing is asked or weird things are asked, exit.
-            #text = ""
-
+        except sr.RequestError as e:
+            tkinter.messagebox.showinfo("Cannot request results from the Speech Recognition")
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            pass
 def explain():
     tkinter.messagebox.showinfo('Shortcut words','Naver, Google, Webtoon, Youtube, or Github')
     answer = tkinter.messagebox.askquestion('!!','Do you Understand?')
